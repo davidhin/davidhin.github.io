45b56245544abde1e47864dc6d3b9583823988b9
davidhin <a1720858@adelaide.edu.au>
2020-10-23 15:30:54 +1030

    Update vuln2.py

--- vuln2.py
+++ vuln2.py
@@ -11,3 +11,13 @@ import dot-prop # FAKE - From NPM
 import lodash # FAKE - From NPM
 import pulse_connect_secure # FAKE - From NPM
 from openapi import create_spec # MOCK - Should be openapi_core, but fuzzymatching doesn't detect it
+from openapi import create_spec # MOCK - Should be openapi_core, but fuzzymatching doesn't detect it
+from openapi import create_spec # MOCK - Should be openapi_core, but fuzzymatching doesn't detect it
+from openapi import create_spec # MOCK - Should be openapi_core, but fuzzymatching doesn't detect it
+import json_pattern_validator # FAKE - From NPM
+import dot-prop # FAKE - From NPM
+from openapi import create_spec # MOCK - Should be openapi_core, but fuzzymatching doesn't detect it
+from openapi import create_spec # MOCK - Should be openapi_core, but fuzzymatching doesn't detect it
+from openapi import create_spec # MOCK - Should be openapi_core, but fuzzymatching doesn't detect it
+import json_pattern_validator # FAKE - From NPM
+import dot-prop # FAKE - From NPM
